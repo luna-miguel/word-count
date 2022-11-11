456a2031184c47323ed85d77d83f90b94fd60af8
mluna1024 <mluna006@citymail.cuny.edu>
2022-11-11 10:49:16 -0500

    Implemented WordFrequency class

--- word-count.py
+++ word-count.py
@@ -1,2 +1,18 @@
-msg = "Hello world"
-print(msg)
+# Python word counter
+# CSC 11300 - Programming language
+# Miguel Luna
+
+
+# Class that holds a given word and the frequency of that word in a text.
+class WordFrequency:    
+
+    def __init__(self, word, frequency = 1): # constructor
+        if type(word) != str:
+           raise ValueError("Word must be of a string type.") 
+        self.word = word
+        if frequency == 0:
+            raise ValueError("Frequency of a new word cannot be zero.")
+        self.frequency = frequency
+
+    def __str__(self): # string overloading function
+        return self.word
